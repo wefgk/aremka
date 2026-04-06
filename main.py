64c9e294ace7df5588086aad65d4d33bb8e69b63
wefgk <gavgavded@gmail.com>
2026-04-06 08:49:09 +0300

    какашечки иууууууу

---
+++ main.py
@@ -0,0 +1,19 @@
+def calculate_addition(addend_1: float, addend_2: float) -> float:
+    total = addend_1 + addend_2
+    return total
+
+def calculate_subtraction(minuend: float, subtrahend: float) -> float:
+    difference = minuend - subtrahend
+    return difference
+
+def calculate_multiplication(factor_1: float, factor_2: float) -> float:
+    product = factor_1 * factor_2
+    return product
+
+def calculate_division(dividend: float, divisor: float) -> float:
+    quotient = dividend / divisor
+    return quotient
+
+def calculate_exponentiation(base: float, power: float) -> float:
+    result = base ** power
+    return result
